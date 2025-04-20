4ea1918b7314dff674d1cee2d751f021047f4b28
alegabas <alegabas@gmail.com>
2025-04-20 14:48:08 +0000

    novato_navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -14,9 +14,32 @@ int main() {
             tabuleiro[i][j] = 0;
         }
     }
+ 
+    // POSICIONAMENTO DOS NAVIOS
 
+    // Posiciona o Navio 1 na Horizontal (Linha 3 - Colunas D, E e F)
+
+    int linha1 = 2; // Linha 3 (índice 2)
+    int coluna1 = 3; // Começa na coluna D (índice 3)
+    int cont = 0; // Conta as posições já preenchidas a partir do índice
+        while (cont < 3) {
+            tabuleiro[linha1][coluna1 + cont] = 3;
+            cont++;
+        }
+
+    // Posiciona o Navio 2 na Vertical (Coluna H - Linhas 6, 7 e 8)
+    
+    int coluna2 = 7; // Coluna H (índice 7)
+    int linha2 = 5; // Começa na linha 6 (índice 5)
+    cont = 0; // Conta as posições já preenchidas a partir do índice
+        while (cont < 3) {
+            tabuleiro[linha2 + cont][coluna2] = 3;
+            cont++;
+        }
+
+ 
     // Cabeçalho com letras de A a J representando as colunas
-    printf("    ");
+    printf("    "); // Espaçamento para alinhar as colunas corretamente
     for (j = 0; j < 10; j++) {
         printf(" %c ", 'A' + j);
     }
